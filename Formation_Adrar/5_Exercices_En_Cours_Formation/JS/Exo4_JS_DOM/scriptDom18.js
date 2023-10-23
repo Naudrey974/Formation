149c275ea8cc84286a47c6b1af29f20b9fdf989b
Audrey <audrey.simon@live.fr>
2023-10-23 21:52:53 +0200

    exo18 js bonus maison

--- Formation_Adrar/5_Exercices_En_Cours_Formation/JS/Exo4_JS_DOM/scriptDom18.js
+++ Formation_Adrar/5_Exercices_En_Cours_Formation/JS/Exo4_JS_DOM/scriptDom18.js
@@ -1,20 +1,24 @@
 
 
 function addTask(){  
-    /* const task=document.querySelector("#task").value;
-    const newTask=document.createElement('p');
-    newTask.textContent=task;
-    tasks.appendChild(newTask); */
-    const task=document.querySelector("#task").value;
-    task.setAttribut('id',container);
-    const newTask=document.createElement('p');
-    newTask.textContent=task;
-    const bt1=document.createElement('button');
+     const task=document.querySelector("#task").value;
+    //const newTask=document.createElement('p');
+    //newTask.textContent=task;
+    //tasks.appendChild(newTask); 
+let container=0;
+const element=document.getElementById('tasks');
+element.setAttribute('id', container);
+container=document.createElement('p');
+container.textContent=task;
+tasks.appendChild(container); 
+    
+
+    /* const bt1=document.createElement('button');
     const bt2=document.createElement('button');
     bt1.setAttribute('id',delete);
     bt1.setAttribute('onclick',deleteTask(this));
     bt2.setAttribute('id',update);
-    bt2.setAttribute('onclick',updateTask(this))
+    bt2.setAttribute('onclick',updateTask(this)) */
 }
 
 
