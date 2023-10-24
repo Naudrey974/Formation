ca6f232826aae67446209c77fa9e736c39ab1436
Audrey <audrey.simon@live.fr>
2023-10-24 22:56:41 +0200

    exo DOM20 maison

--- Formation_Adrar/5_Exercices_En_Cours_Formation/JS/Exo4_JS_DOM/scriptDom20.js
+++ Formation_Adrar/5_Exercices_En_Cours_Formation/JS/Exo4_JS_DOM/scriptDom20.js
@@ -1,4 +1,68 @@
-//function pour mettre à jour le texte de la tache courante
+//récupération de la div (tasks)
+const liste = document.querySelector('#tasks');
+
+//création de la clé "tasks" avec en valeur un tableau vide
+localStorage.setItem("tasks","[]");
+
+//fonction pour ajouter une tâche
+
+function addTask(){
+    //récupération de l'input (task)
+    const valeur = document.querySelector('#task').value;    
+    localStorage.setItem("tasks",valeur);
+      
+    //création du container (container)
+    const container = document.createElement('div');
+    //ajout de l'attribut class : container (div container)
+    container.setAttribute('class', 'container');
+    //création du paragraphe
+    const paragraphe = document.createElement('p');
+    paragraphe.textContent = valeur;
+    const btUpdate = document.createElement('button');
+    //création du bouton update
+    btUpdate.textContent = "Update";
+    btUpdate.setAttribute('onclick','updateTask(this)');
+    btUpdate.setAttribute('id','update');
+    //création du bouton delete
+    const btDelete = document.createElement('button');
+    btDelete.textContent = "Delete";
+    btDelete.setAttribute('onclick','deleteTask(this)');
+    //ajouter les enfants à la div (container)
+    container.appendChild(paragraphe);
+    container.appendChild(btUpdate);
+    container.appendChild(btDelete);
+    //ajouter la div container à liste
+    liste.appendChild(container);
+}
+//Exo 20 fonction sur le LocalStorage "showAllTasks"
+function showAllTask(){
+    const storage=localStorage.getItem("tasks");
+    console.log(storage);
+    const paragraphe = document.createElement('p');
+    paragraphe.textContent = storage;
+    const container = document.createElement('div');
+    container.setAttribute('class', 'container');
+    container.appendChild(paragraphe);
+    liste.appendChild(container);
+    console.log(liste)
+    //delAllTask();
+}
+//fonction qui va supprimer tous les enfants de tasks
+function delAllTask(){
+    while(liste.firstChild){
+        liste.removeChild(liste.firstChild);
+    }
+}
+//fonction pour recharger la page
+function reload(){
+    location.reload();
+}
+//fonction pour supprimer la tache (div container parente)
+function deleteTask(e){
+    e.parentNode.remove();
+}
+//fonction pour modifier la tâche
+let statut=true;
 function updateTask(e){
     //test si statut est égale à true
     if(statut){
